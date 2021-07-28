388ce40b45af26a4184ae0698b34f7c79543f1e7
elayabharathj <55911029+elayabharathj@users.noreply.github.com>
2021-07-28 12:43:58 +0530

    Update PostRepository.java
    Added comments

--- src/main/java/com/guestbook/guestapp/repository/PostRepository.java
+++ src/main/java/com/guestbook/guestapp/repository/PostRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends CrudRepository<Post, Integer> {
+    //Implementation has been provided by the class SimpleJpaRepository of Springframwwork data
+    //All dao calls are accessed through SimpleJpaRepository
+
 }
