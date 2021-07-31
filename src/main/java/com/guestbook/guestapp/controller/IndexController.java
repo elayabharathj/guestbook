59a9f62f2a676d566a3a61c3ab02c2c563803393
elayabharathj <55911029+elayabharathj@users.noreply.github.com>
2021-07-31 15:22:54 +0530

    Update IndexController.java
    Added logout

--- src/main/java/com/guestbook/guestapp/controller/IndexController.java
+++ src/main/java/com/guestbook/guestapp/controller/IndexController.java
@@ -4,10 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 
 @Controller
@@ -22,11 +26,22 @@ public class IndexController {
         String username = authentication.getName().toString();
         String role = authentication.getAuthorities().stream().findAny().get().toString();
         LOGGER.info("Role/Authority obtained from authentication for user <--> {} is <--> {}", username, role);
+        model.addAttribute("name", "Welcome " + username +"!!!");
         if ("ROLE_ADMIN".equals(role)) {
             return "redirect:/posts";
         }
-        model.addAttribute("name", username);
         LOGGER.info("Leaving index()");
         return "index";
+    }
+
+    @GetMapping(value="/logout")
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        LOGGER.info("Entering into logoutPage()");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        LOGGER.info("Leaving logoutPage()");
+        return "redirect:/login?logout";
     }
 }
